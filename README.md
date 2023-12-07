af2bcbb22d21de0efffff9f2ad9d3bfcb9427fd8
Alicia Wood <alicia.crowder@gmail.com>
2023-12-07 11:39:14 -0700

    in class starter code

---
+++ README.md
@@ -0,0 +1,24 @@
+# in-class-dec-7
+
+
+To "pull" this to your local machine, run the following:
+
+```git clone git@github.com:momscodeacademy/in-class-dec-7.git```
+
+Then to create and switch to a new branch, run the following:
+
+```git checkout -b [branch-name-here]```
+
+
+Once you finish the assignment you will do the normal
+
+```git add .```
+
+```git commit -m "your message here"```
+
+```git push```
+
+
+Then you will go to github at that repository and create a new pull request!
+
+
---
+++ script.js
@@ -0,0 +1,19 @@
+const cityPopulations = {
+    "New York": 8623000,
+    "Los Angeles": 3990456,
+    "Chicago": 2705994,
+    "Houston": 2325502,
+    "Phoenix": 1680992
+  };
+
+  
+
+// Function to calculate total population
+function calculateTotalPopulation(populations) {
+    // Write code here to calculate the total population using a for...in loop
+    
+}
+
+const totalPopulation = calculateTotalPopulation(cityPopulations);
+console.log("Total population:", totalPopulation); // Output: Total population: 19375944
+  
